fa1c92a7d28371b6ba4bc1f7e067558da5097b5d
Johan Doornenbal <johan@filternet.nl>
2015-01-03 18:12:50 +0100

    find for persons ready

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,7 +10,16 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', ['$scope', 'RestCollectionFindService', function($scope, RestCollectionFindService) {
+/*
+	var allPersonsUrl = 'http://xtalus.apps.gedge.nl/simple/restful/services/Persons/actions/allPersons/invoke';
+	RestCollectionFindService.restObject(allPersonsUrl).then(
+		function(data){
+			$scope.allPersons = data;
+		}
+	);
+*/
 	var url = 'http://xtalus.apps.gedge.nl/simple/restful/services/Persons/actions/findPersonsContains/invoke';
+/*
 	$scope.test = url;
 	var zoekobject = {"achternaam(wildcards*Toegestaan)":"o"};
 	RestCollectionFindService.restObject(url,zoekobject).then(
@@ -18,5 +27,23 @@ angular.module('myApp.view1', ['ngRoute'])
 			$scope.test = data;
 		}
 	);
+*/
+	
+	$scope.searchAction = function(){
+		$scope.test1=$scope.searchstring;
+		var zoekobject ={};
+		zoekobject["achternaam(wildcards*Toegestaan)"] = $scope.searchstring;
+		RestCollectionFindService.restObject(url,zoekobject).then(
+			function(data){
+				$scope.results = data;
+			}
+		);
+	};
+	$scope.$watch('searchstring', function(newVal, oldVal, scope) {
+		if (newVal !== oldVal) {
+			$scope.searchAction();
+			$scope.showResults='show';
+		};
+	});
 	
 }]);
