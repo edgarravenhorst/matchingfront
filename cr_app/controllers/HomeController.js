3eaf732f4ed9aac6e3f4d5743ac9560a50dd9cfb
Edgar Ravenhorst <info@edge-art.nl>
2015-02-05 13:44:59 +0100

    Test collection

--- cr_app/controllers/HomeController.js
+++ cr_app/controllers/HomeController.js
@@ -3,33 +3,42 @@ define(function(require) {
     function HomeController($scope, RestService){
         this.$inject = ['$scope', 'RestService'];
 
+        console.log('>> Person & Tag members');
         RestService.getServices(['Tags', 'Persons'])
         .then(function(data){
-
+            console.log('----------------------------------------------------------------------');
             var persons = data.Persons
             var tags = data.Tags
 
             persons.thisIsYou.post().then(function(result){
-                var profile = result[0]
+                var profile =
 
                 profile = RestService.initRestObject(result[0].data);
+                console.log('--------------------');
+                console.log('>> This is you members: ');
                 console.log(profile)
+                console.log('----------------------------------------------------------------------');
 
-                console.log('roles: ');
+                console.log('>> profile roles: ');
                 console.log(profile.roles)
+                console.log('----------------------------------------------------------------------');
 
-                console.log('demands: ');
 
                 profile.myDemands.get().then(function(demands){
+                    console.log('--------------------');
+                    console.log('>> profile demands: ');
                     console.log(demands);
+                    console.log('----------------------------------------------------------------------');
                 });
 
                 return result
             });
 
+
             tags.allTags.post().then(function(result){
                 console.log('All tags: ');
                 console.log(result);
+                console.log('----------------------------------------------------------------------');
 
                 $scope.tags = result;
             });
