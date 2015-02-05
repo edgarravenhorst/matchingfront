4ded76fd64dad301132e59ac753be403aa29f35f
Edgar Ravenhorst <info@edge-art.nl>
2015-02-05 11:12:56 +0100

    Cleanup Homecontroller

--- cr_app/controllers/HomeController.js
+++ cr_app/controllers/HomeController.js
@@ -3,41 +3,19 @@ define(function(require) {
     function HomeController($scope, RestService){
         this.$inject = ['$scope', 'RestService'];
 
-        $scope.searchQuery = 'rijn';
-
-       RestService.getServices(['Persons', 'Organisations', 'Tags'])
-
+        RestService.getServices(['Organisations', 'Tags'])
         .then(function(data){
-           console.log(data);
+            console.log(data);
 
             var persons = data.Persons
             var tags = data.Tags
 
             tags.allTags.post().then(function(result){
-                //console.log('All tags: ');
-                //console.log(result);
+                console.log('All tags: ');
+                console.log(result);
 
                 $scope.tags = result;
-
             });
-
-            persons.allPersons.post().then(function(result){
-                //console.log('All Persons: ');
-                //console.log(result);
-            });
-
-
-            $scope.test = persons.findPersons.post({lastName:$scope.searchQuery}).then(function(person){
-
-
-                var data = person[0].data.members
-                $scope.results =  [{value: data.firstName.value + ' ' + data.middleName.value + ' ' + data.lastName.value}]
-
-                return [{value: data.firstName.value + ' ' + data.middleName.value + ' ' + data.lastName.value}]
-            });
-
-           console.log(persons.findPersons.post({lastName:$scope.searchQuery}));
-           $scope.test = persons.findPersons.post({lastName:$scope.searchQuery})
         })
     };
 
